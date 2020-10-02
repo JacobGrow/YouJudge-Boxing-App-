98a4488ed5eebd110412d9675c2d34fbd6d38f7d
Jacob Grow <jacobagrow@gmail.com>
2020-10-02 15:25:02 -0600

    Fixed fight model

--- server/models/Fight.js
+++ server/models/Fight.js
@@ -1,14 +1,15 @@
 import mongoose from "mongoose";
 const Schema = mongoose.Schema;
 
-const Round = new Schema(
+const Fight = new Schema(
   {
     title: { type: String, required: true },
     fighter1: { type: String, required: true },
-    score2: { type: String, required: 2 },
-    description: { type: String }
+    fighter2: { type: String, required: 2 },
+    description: { type: String },
+    date: { type: Date }
   },
   { timestamps: true, toJSON: { virtuals: true } }
 );
 
-export default Round;
+export default Fight;
